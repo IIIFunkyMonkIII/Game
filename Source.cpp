ddcaf52caf1a65839b02f1cfc68e627180f7b93f
IIIFunkyMonkIII <paj.soderlind@gmail.com>
2022-01-11 13:09:01 +0100

    nya ändringar gör om så att den jämför början på commando med de olika cmd

--- Source.cpp
+++ Source.cpp
@@ -65,9 +65,7 @@ struct Item
 
 struct Room					//Innehåll i rum
 {
-	Item items[5]{}; // Dynamisk Array här sen **************************************************
-
-
+	Item items[5]{}; // dynamisk Array här sen **************************************************
 
 };
 
@@ -80,7 +78,7 @@ Room closetContent;
 
 //Forward Declarations
 void roomList();
-void command();
+void command(Room x);
 void purge();
 void closet();
 void start();
@@ -140,12 +138,13 @@ void start()
 
 void command(Room roomContent) // **************' Gör så att man kan ändra room content globalt istället för att ha det i en funktion för att passa med referens sög lite puck
 {
+	std::cin.ignore(1);
+
 	std::string currentCommand{};
-	std::cin.ignore(1000);
 	getline(std::cin, currentCommand);
-	
+	std::cout << currentCommand;
 	std::string wholeCommand{};
-	int contentArraySize{ (5 - 1) };					//gör till dynamisk array eller vector.
+	int contentArraySize{ 5 };//gör till dynamisk array eller vector.
 
 	for (int loop{0}; loop < contentArraySize; loop++) // pick up command
 	{
@@ -158,14 +157,18 @@ void command(Room roomContent) // **************' Gör så att man kan ändra room 
 			{
 					oscar.backpack.itemSlotFull = true;						//ryggsäck nu full
 					oscar.backpack.itemName = roomContent.items[0].itemName;	//den innehåller en item som heter 
-					roomContent.items[0].itemAvailability == false;	//itemet finns nu inte tillgängligt i rummet
+					roomContent.items[0].itemAvailability = false;	//itemet finns nu inte tillgängligt i rummet
 
-					std::cout << "Your xxx olympics backpack now contains " << oscar.backpack.itemName;
+					std::cout << "Your xxx olympics backpack now contains" << oscar.backpack.itemName << '\n';
 			}
 			else
 			{
-				std::cerr << "That item isn't available! \n";
+					std::cerr << "Your back pack is full. Empty the backpack and then try again! \n";
 			}
+		}
+		if (currentCommand != "pick up " && currentCommand ) // fortsätt
+		{
+			std::cerr << "That item isn't available! \n";
 		}
 	}
 
@@ -177,7 +180,7 @@ void command(Room roomContent) // **************' Gör så att man kan ändra room 
 			oscar.backpack.itemName = "";
 			oscar.backpack.itemAbility = "";
 		}
-		ëlse
+		else
 		{
 			std::cout << "Your backpack is already empty! \n";
 		}
@@ -203,10 +206,6 @@ void closet()
 
 	if (oscar.firstTimeInCloset == true)
 	{
-		switch (currentRoomDialogSegment)
-		{
-		case 1:
-		{
 			oscar.firstTimeInCloset = false;
 
 			//Closet start item
@@ -214,26 +213,20 @@ void closet()
 			closetContent.items[0].itemClass = "backpack";
 
 			std::cout << "You lie knocked out on the floor in a confined space under a pile of clothes" << '\n'
-				<< "You see a pair of bamboo boxershorts that your uncle manufactures write pickup boxers to pick up the bamboo boxershorts that is lying besides you." << '\n'
-				<< "This item is of type " << '\n' << closetContent.items[0].itemClass;
-				<< "You only have one itemslot in your xxx olypics backpack if you wish to pick up another item write empty backpack and then pick up..." << '\n'
-				<< "Now try pick upp and drop the boxers\n" << '\n';
+				<< "You see a pair of bamboo boxershorts that your dads cousin manufactures write pickup boxers to pick up the bamboo boxershorts that is lying besides you." << '\n'
+				<< "This item is of type " << closetContent.items[0].itemClass << '\n';
+				std::cout << "You only have one itemslot in your xxx olypics backpack if you wish to pick up another item write empty backpack and then pick up..." << '\n'
+				<< "Now try pick up and drop the boxers\n" << '\n';
 
-				currentRoomDialogSegment += 1;
-		}
-		case 2:
-		{
+
+
 			while (oscar.backpack.itemSlotFull != true)
 			{
 				command(closetContent);
-				
 			}
 			
 			std::cout << "Good you picked up the boxers, now try to drop them again \n";
-			currentRoomDialogSegment += 1;
-		}
-		case 3: 
-		{
+
 			while (oscar.backpack.itemSlotFull != false)
 			{
 				command(closetContent);
@@ -241,16 +234,12 @@ void closet()
 			}
 
 			std::cout << "Good you dropped the underware. You don't want to walk around with those all the time.";
-			currentRoomDialogSegment += 1;
-
-		}
-
-		}
-
 
 	}
 
 }
+
+
 
 void purge()
 {
