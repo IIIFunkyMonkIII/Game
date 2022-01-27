4029e2a01c8546879465f57961deff6db29ce39d
IIIFunkyMonkIII <paj.soderlind@gmail.com>
2022-01-27 20:28:33 +0100

    fixa svante

--- Source.cpp
+++ Source.cpp
@@ -9,8 +9,8 @@
 //Globals
 int X{ 1 };
 int Y{ 2 };
-int Z{ 2 };
-									//the room we are in
+int Z{ 2 };							//Array for the contents of the rooms
+int currentRoom{0};					//the room we are in
 int currentRoomDialogSegment{ 1 };	// The dialog segment
 
 //Structs
@@ -77,20 +77,19 @@ struct Room					//Innehåll i rum
 
 //Global struct initalizations
 Character oscar;
-Room closetContent;
-
+Room roomContent[7]{};
 
 
 //Forward Declarations
 void roomList();
-void command(Room x);
+void command();
 void purge();
 void closet();
 void start();
 void svante();
 void oscarsRoom();
-void compactArray(Room roomContent, int removedItem, int contentArraySize);
-int lastItemInContentArray(Room roomContent, int contentArraySize);
+void compactArray(int removedItem, int contentArraySize);
+int lastItemInContentArray(int contentArraySize);
 void next();
 
 
@@ -146,32 +145,32 @@ void start()
 	return;
 }
 
-void command(Room roomContent) // måste göra en bättre lösning på Room och roomContent så att command blir en kravlös funktion
+void command() // måste göra en bättre lösning på Room och roomContent så att command blir en kravlös funktion
 {
 	std::string currentCommand{};
 	getline(std::cin, currentCommand);
 	std::string wholeCommand{};
-	int contentArraySize{ 5 };//gör till dynamisk array eller vector.
+	int size{ 5 };																//gör om ork finns till dynamisk array eller vector.
 	bool commandFound{false};
 
-	// gör en if för om början på commando == pick up
+	
 	if (currentCommand.substr(0, 7) == "pick up")								// pick up command
 	{
-		for (int loop{ 0 }; loop < contentArraySize; loop++)					//Loop för att jämföra items
+		for (int loop{ 0 }; loop < size; loop++)								//Loop för att jämföra items
 		{
-			wholeCommand = ("pick up " + roomContent.items[loop].itemName);
+			wholeCommand = ("pick up " + roomContent[currentRoom].items[loop].itemName);		// sätter ihop en sök mening med pick up och det loopade itemet
 
-			if (currentCommand == wholeCommand && roomContent.items[loop].itemClass == "backpack")
+			if (currentCommand == wholeCommand && roomContent[currentRoom].items[loop].itemClass == "backpack")
 			{																	//System för att kolla vilka items som är tillgängliga samt system för att plock upp skiten och sätta att den itne är tillgänglig längre.
 				if (oscar.backpack.itemSlotFull == true)
 				{
 					std::cerr << "Your backpack already contains one item. Empty the backpack and then try again! \n";
 				}
-				else if (roomContent.items[loop].itemAvailability == true && oscar.backpack.itemSlotFull == false)
+				else if (roomContent[currentRoom].items[loop].itemAvailability == true && oscar.backpack.itemSlotFull == false)
 				{				
 					oscar.backpack.itemSlotFull = true;							//ryggsäck nu full
-					oscar.backpack.itemName = roomContent.items[loop].itemName;	//den innehåller en item som heter 
-					roomContent.items[loop].itemAvailability = false;				//itemet finns nu inte tillgängligt i rummet
+					oscar.backpack.itemName = roomContent[currentRoom].items[loop].itemName;	//den innehåller en item som heter 
+					roomContent[currentRoom].items[loop].itemAvailability = false;				//itemet finns nu inte tillgängligt i rummet
 
 					std::cout << "Your xxx olympics backpack now contains " << oscar.backpack.itemName << '\n';
 				}
@@ -179,17 +178,17 @@ void command(Room roomContent) // måste göra en bättre lösning på Room och roomC
 				commandFound = true;
 			}	
 
-			if (currentCommand == wholeCommand && roomContent.items[loop].itemClass == "weapon")
+			if (currentCommand == wholeCommand && roomContent[currentRoom].items[loop].itemClass == "weapon")
 			{																	//System för att kolla vilka items som är tillgängliga samt system för att plock upp skiten och sätta att den itne är tillgänglig längre.
 				if (oscar.weapon.weaponSlotFull == true)
 				{
 					std::cerr << "You are already holding a weapon. Drop it and try again! \n";
 				}
-				else if (roomContent.items[loop].itemAvailability == true && oscar.weapon.weaponSlotFull == false)
+				else if (roomContent[currentRoom].items[loop].itemAvailability == true && oscar.weapon.weaponSlotFull == false)
 				{
 					oscar.weapon.weaponSlotFull = true;							//ryggsäck nu full
-					oscar.weapon.weaponName = roomContent.items[0].itemName;	//den innehåller en item som heter 
-					roomContent.items[loop].itemAvailability = false;			//itemet finns nu inte tillgängligt i rummet
+					oscar.weapon.weaponName = roomContent[currentRoom].items[0].itemName;	//den innehåller en item som heter 
+					roomContent[currentRoom].items[loop].itemAvailability = false;			//itemet finns nu inte tillgängligt i rummet
 
 					std::cout << "Your hand now contains" << oscar.weapon.weaponName << '\n';
 				}
@@ -197,26 +196,26 @@ void command(Room roomContent) // måste göra en bättre lösning på Room och roomC
 				commandFound = true;
 			}
 
-			if (currentCommand == wholeCommand && roomContent.items[loop].itemClass == "potion")
+			if (currentCommand == wholeCommand && roomContent[currentRoom].items[loop].itemClass == "potion")
 			{																	//System för att kolla vilka items som är tillgängliga samt system för att plock upp skiten och sätta att den itne är tillgänglig längre.
 				if (oscar.potion.potionSlotFull == true)
 				{
 					std::cerr << "Your back pack is full. Empty the backpack and then try again! \n";
 				}
-				else if (roomContent.items[loop].itemAvailability == true && oscar.potion.potionSlotFull == false)
+				else if (roomContent[currentRoom].items[loop].itemAvailability == true && oscar.potion.potionSlotFull == false)
 				{
 					oscar.potion.potionSlotFull = true;							//ryggsäck nu full
-					oscar.potion.potionName = roomContent.items[0].itemName;	//den innehåller en item som heter 
-					roomContent.items[loop].itemAvailability = false;			//itemet finns nu inte tillgängligt i rummet
+					oscar.potion.potionName = roomContent[currentRoom].items[0].itemName;	//den innehåller en item som heter 
+					roomContent[currentRoom].items[loop].itemAvailability = false;			//itemet finns nu inte tillgängligt i rummet
 
 					std::cout << "Your xxx olympics backpack now contains" << oscar.potion.potionName << '\n';
 				}
 				commandFound = true;
 			}
 			
-			if (commandFound == true && roomContent.items[loop].itemAvailability == false)
+			if (commandFound == true && roomContent[currentRoom].items[loop].itemAvailability == false)
 			{
-				compactArray(roomContent, loop, contentArraySize);
+				compactArray(loop, size);
 			}		
 		}
 
@@ -225,16 +224,24 @@ void command(Room roomContent) // måste göra en bättre lösning på Room och roomC
 			std::cout << "That item doesn't exist! \n \n";
 		}
 	}
-		
-
 
 	if (currentCommand == "empty backpack")
 	{
-		if (oscar.backpack.itemSlotFull == true)
+		int lastElement{ lastItemInContentArray(size) + 1 }; // +1 för att få sista lediga
+
+		if (oscar.backpack.itemSlotFull == true && lastElement <= 5)
 		{
+			roomContent[currentRoom].items[lastElement].itemName = oscar.backpack.itemName;
+			roomContent[currentRoom].items[lastElement].itemClass = "backpack";
+				roomContent[currentRoom].items[lastElement].itemAvailability = true;
+
 			oscar.backpack.itemSlotFull = false;
 			oscar.backpack.itemName = "";
-			oscar.backpack.itemAbility = "";
+			oscar.backpack.itemAbility = "";		
+		}
+		else if (lastElement > 5)
+		{
+			std::cout << "This room is too cluttered! Clean up or leave it some where else. \n";
 		}
 		else
 		{
@@ -250,10 +257,12 @@ void roomList() // rumLista för att navigera genom alla rum.
 	if(Z == 2 && Y == 2 && X == 1)
 	{
 		closet();
+		currentRoom = 0;
 	}
 	if (Z == 2 && Y == 2 && X == 2)
 	{
 		oscarsRoom();
+		currentRoom = 1;
 	}
 
 
@@ -268,14 +277,14 @@ void closet()
 			oscar.firstTimeInCloset = false;
 
 			//Closet start item
-			closetContent.items[0].itemName = "boxers";
-			closetContent.items[0].itemClass = "backpack";
-			closetContent.items[1].itemName = "butterKnife";
-			closetContent.items[1].itemClass = "weapon";
+			roomContent[currentRoom].items[0].itemName = "boxers";
+			roomContent[currentRoom].items[0].itemClass = "backpack";
+			roomContent[currentRoom].items[1].itemName = "butterKnife";
+			roomContent[currentRoom].items[1].itemClass = "weapon";
 
 			std::cout << "You lie knocked out on the floor in a confined space under a pile of clothes" << '\n'
 			<< "You see a pair of bamboo boxershorts that your dads cousin manufactures write pickup boxers to pick up the bamboo boxershorts that is lying besides you." << '\n'
-			<< "This item is of type " << closetContent.items[0].itemClass << '\n';
+			<< "This item is of type " << roomContent[currentRoom].items[0].itemClass << '\n';
 			std::cout << "You only have one itemslot in your xxx olypics backpack if you wish to pick up another item write empty backpack and then pick up..." << '\n'
 			<< "Now try pick up and drop the boxers\n" << '\n';
 
@@ -283,14 +292,14 @@ void closet()
 
 			while (oscar.backpack.itemSlotFull != true)
 			{
-				command(closetContent);
+				command();
 			}
 			
 			std::cout << "Good you picked up the boxers, now try to drop them again \n";
 
 			while (oscar.backpack.itemSlotFull != false)
 			{
-				command(closetContent);
+				command();
 
 			}
 
@@ -316,7 +325,7 @@ void oscarsRoom()
 			"In the chair lies a hairy beast called Svante and with a swift motion he lunges at you for an attack! \n \n";
 
 		std::cout << "What path do you choose: attack or cuddles?: ";
-
+		std::cout << "Hell";
 		svante(); //Fortsätt Svante funktionen.
 
 	}
@@ -325,7 +334,8 @@ void oscarsRoom()
 void svante()
 {
 	std::string path{};
-	
+	std::cout << path;
+
 	while (path != "cuddles" || path != "attack")
 	{
 		std::cin >> path;
@@ -335,17 +345,13 @@ void svante()
 			std::cout << "You chose to embrace the terrible beast and he now follows you because he want's food down in the kitchen \n" <<
 				"Find kibble for Svante within 10 commands or he will eat you!\n" << "The kitchen is down stairs, walk out of the room and you should find them.";
 			
-			for (int loop{0}; X != 6 && Y != 2 && Z != 1 && loop <= 10; loop++)
-			{
-				command();
-			}
-
+			// få while till command att fungera tills objektivet är uppfyllt
 		}
 		else if (path == "attack")
 		{
-			std::cout << "You chose to confront the beast and much like in Harry Potter the chambre of secrets you win the fight with the power of love. \n" 
+			std::cout << "You chose to confront the beast and much like in Harry Potter the chambre of secrets you win the fight with the power of love. \n"
 				<< "Svante gets smited in to oblivion and you win the fight but you loose he power of love ability. \n"
-				<< "It's soon time for school and you better get ready by walking there.":
+				<< "It's soon time for school and you better get ready by walking there.";
 
 		}
 		else
@@ -356,27 +362,27 @@ void svante()
 	}
 }
 
-void compactArray(Room roomContent, int loop, int contentArraySize)
+void compactArray(int loop, int contentArraySize)
 {
-	int lastItemInList{ lastItemInContentArray(roomContent, contentArraySize) };
+	int lastItemInList{ lastItemInContentArray( contentArraySize) };
 
 	//kopierar över item information till hålet som skapats
-	roomContent.items[loop].itemAvailability = roomContent.items[lastItemInList].itemAvailability;
-	roomContent.items[loop].itemClass = roomContent.items[lastItemInList].itemClass;
-	roomContent.items[loop].itemName = roomContent.items[lastItemInList].itemName;
+	roomContent[currentRoom].items[loop].itemAvailability = roomContent[currentRoom].items[lastItemInList].itemAvailability;
+	roomContent[currentRoom].items[loop].itemClass = roomContent[currentRoom].items[lastItemInList].itemClass;
+	roomContent[currentRoom].items[loop].itemName = roomContent[currentRoom].items[lastItemInList].itemName;
 	//Sätter sista i listan till tom
-	roomContent.items[lastItemInList].itemAvailability = false;
-	roomContent.items[lastItemInList].itemClass = "";
-	roomContent.items[lastItemInList].itemName = "";
+	roomContent[currentRoom].items[lastItemInList].itemAvailability = false;
+	roomContent[currentRoom].items[lastItemInList].itemClass = "";
+	roomContent[currentRoom].items[lastItemInList].itemName = "";
 }
 
-int lastItemInContentArray(Room roomContent, int contentArraySize)
+int lastItemInContentArray( int contentArraySize)
 {
 	int lastItemInList{-1};  // -1 enkel fix för off by one error
 
 	for (int l{ 0 }; l < contentArraySize; l++)
 	{
-		if (roomContent.items[l].itemName != "")
+		if (roomContent[currentRoom].items[l].itemName != "")
 		{
 			lastItemInList++;
 		}
